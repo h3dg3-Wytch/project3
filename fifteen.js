6772be724c128725ce0cb83b3347b7c0da3c21c5
Rex <rexpeters3n@gmail.com>
2016-11-02 21:55:02 -0400

    Add images, and randomization

--- fifteen.js
+++ fifteen.js
@@ -1,30 +1,134 @@
 $(document).ready(function(){
-	//Place jquery here
-	setUpBoard();
+
+	var board = setUpBoard();
+
+	//Creates board, and then draw tiles
+	$("#shuffle").click(function(){
+		board = randomizeGrid(board);
+		drawTiles(board);
+	});
+
+	//todo generate auto add alerts
+	$("#grid").on("click", "#0", function(){
+		alert("0");
+	});
+	$("#grid").on("click", "#1", function(){
+		alert("1");
+	});
+	$("#grid").on("click", "#2", function(){
+		alert("2");
+	});
+	$("#grid").on("click", "#3", function(){
+		alert("3");
+	});
+	$("#grid").on("click", "#4", function(){
+		alert("4");
+	});
+	$("#grid").on("click", "#5", function(){
+		alert("5");
+	});
+	$("#grid").on("click", "#6", function(){
+		alert("6");
+	});
+	$("#grid").on("click", "#7", function(){
+		alert("7");
+	});
+
+
+
+
 });
 
-function setUpBoard(){
-	//we need the context in order to start drawing. 
-	var context = document.getElementById("board").getContext("2d");
-	var img = new Image();
-	img.src = "images/vapor.jpg";
-	//img.addEventListener('load', drawTiles, false);
 
-	var boardSize = $("#board").width();
-	var tileSize = boardSize / 4; 
-	var boardParts = new Object;
-	setPieces();
+function setUpBoard(){
+	board = createGrid();
+	drawTiles(board);
+	return board;
 }
 
-function setPieces(){
-	var randomvalues 
+//Creates inital grid. Storing objects for it to remeber
+function createGrid(){
+	var x = -300;
+	var y = 0;
+
+	var board = new Array(4);
+	var index = 0;
+	for(var i = 0 ; i < 4; i++){
+		board[i] = new Array(4);
+		for(var j = 0; j < 4; j++){
+			board[i][j] =  new Object;
+			board[i][j].x = x;
+			board[i][j].y = y;
+			board[i][j].index = index;
+			x = x + 100;
+			index++;
+		}
+		x = -300;
+		y = y + 100;
+	}
+	return board;
+}
+
+//Randomizes the grid
+function randomizeGrid(array){
+	var randomIndexs = generateRandomArray();
+	
+	var randomIndex = 0;
+	for(var i = 0; i < 4; i++){
+		for(var j = 0; j < 4; j++){
+			array[i][j].index = randomIndexs[randomIndex];
+			randomIndex++;
+		}
+	}
+
+	return array;
+}
+
+function drawTiles(board){
+	//Takes each item from grid, then adds the new index
+	$("#grid").html("");
+	for(var i = 0 ; i < 4; i++){
+		for(var j = 0 ; j < 4; j++){
+			$("#grid").append(imageTile(board[i][j].index,
+				board[i][j].x,
+				board[i][j].y ));
+		}	
+	}
+}
+
+function imageTile(index, locX, locY){
+
+	var div = $("<div></div>");
+	div.addClass("")
+	var container = $("<div></div>");
+
+	container.addClass("imageTile");
+
+	
+	container.css("background-position", locX + "px " + locY + "px");
+	
+
+	if(index != 15){
+		var imageDiv = $("<div></div>");
+		imageDiv.addClass("image");
+		
+		var img = $("<img/>");
+		img.attr("src", "images/"+ index + ".jpg");
+		imageDiv.append(img);
+		container.append(imageDiv);
+	}
+	var numberDiv = $("<div>"+ (index + 1) + "</div>")
+	numberDiv.addClass("numberImage");
+	numberDiv.attr('id', index);
+
+	container.append(numberDiv);
+	return container;
 }
 
 function generateRandomArray(){
-
 	var result = [];
     for(var i = 0; i < 16; i++){
-     	result[i] = i;
+      	result[i] = i;
     } 
 
     result.sort(function() {
@@ -32,7 +136,5 @@ function generateRandomArray(){
     });
 
     return result;
-
 }
-
 
