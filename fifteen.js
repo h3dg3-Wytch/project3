3e234213db68ea61777f7f31a875fb9b5ecd439d
Rex <rexpeters3n@gmail.com>
2016-11-04 16:50:31 -0400

    Add win condition if tiles are all correct

--- fifteen.js
+++ fifteen.js
@@ -6,12 +6,30 @@ $(document).ready(function(){
 	var shuffledClick = false;
 	var solved = false;
 
+	//YOUR TIMER FUNCTION GOES HERE
+	//setTimeout(function(){
+		//function here
+	//}, time to you lose right here );
+
+
 	//Creates board, and then draw tiles
 	$("#shuffle").click(function(){
 		shuffledClick = true;
 		board = randomizeGrid(board);
 		drawTiles(board);
+		
 	});
+
+	$("#demo").click(function(){
+
+		shuffledClick = true;
+		board =  createGrid();
+		board[2][3].index = parseInt(15);
+		board[3][3].index = parseInt(11);
+		drawTiles(board); 
+		
+	});
+
 
 
 	//todo generate auto add alerts
@@ -23,16 +41,23 @@ $(document).ready(function(){
 			$(this).css({'color' : "", 'text-decoration': "", "opacity": ".2" });
 		}else if(event.type == "click"){
 			if(shuffledClick){
-				board = checkAnswer(board, $(this).attr("id"));
 				console.log(board);
+				board = checkAnswer(board, $(this).attr("id"));
 				drawTiles(board);
-				//check answer
+				solved = isSolved(board);
+				if(solved){
+				//implemnt you won condition, set the inner html of the #grid to something
+					console.log("you won");
+				}
+				
 			}
 		}
 	});
+
 });
 
 function checkAnswer(board, tileId){
+	console.log(board);
 	for(var i = 0 ; i < board.length; i++){
 		for(var j = 0; j < board.length; j++){
 			if(board[i][j].index == tileId){
@@ -52,6 +77,7 @@ function checkAnswer(board, tileId){
 }
 
 function checkSwap(array, x, y, i, j, tileId){
+	console.log(array);
 	var blankTilePosition = getPositionOfLastElement(array);
 	//calculate the distance of the selected tile to the blank tile
 	var distanceFromBlankTile = distance(array[blankTilePosition.i][blankTilePosition.j].x,
@@ -59,11 +85,8 @@ function checkSwap(array, x, y, i, j, tileId){
 	 array[i][j].x,
 	 array[i][j].y); 
 	if(distanceFromBlankTile == 100 && tileId != ((array.length * array.length) - 1 )){
-		console.log("Can swap!");
 		array[i][j].index = ((array.length * array.length) - 1 );
 		array[blankTilePosition.i][blankTilePosition.j].index = parseInt(tileId);
-
-		// array[i][j].index =   ((array.length * array.length) - 1 );
 	}
 
 	return array;
@@ -197,4 +220,19 @@ function generateRandomArray(){
 
     return result;
 }
+
+function isSolved(board){
+	var index = 0;
+	for(var i = 0; i < board.length; i++){
+		for(var j = 0; j < board[i].length; j++){
+			if(board[i][j].index == index){
+				index++;
+			}else{
+				return false;
+			}
+		}
+	}
+	return true;
+}
+
 
